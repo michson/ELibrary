2899ef82d026e4a5dd8a71881540386288196e09
Olaoye Mich <41102062+michson@users.noreply.github.com>
2022-10-20 06:36:00 +0100

    Update Form2.vb

--- fedpoffa_digital_library/Form2.vb
+++ fedpoffa_digital_library/Form2.vb
@@ -2,7 +2,6 @@
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'LibraryDataSet2.library' table. You can move, or remove it, as needed.
         Me.LibraryTableAdapter.Fill(Me.LibraryDataSet2.library)
-
     End Sub
 
     Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
